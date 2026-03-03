d6edd7cc991395c7ecd02bde5aed89a8e4dfd163
Nxghtmare <49699880+xnovos-dev@users.noreply.github.com>
2026-03-03 13:57:47 +0000

    Removed test DB added DB to gitignore

---
+++ commands/Welcomer.py
@@ -0,0 +1,24 @@
+import discord
+from discord.ext import commands
+from discord import app_commands
+from utils.views.welcomer import WelcomerPanel
+from utils.db.functions import welcomer as db
+
+COG = True
+
+
+class WelcomerPanelCog(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        db.initialize_welcomer_table()
+
+    @app_commands.command(name="welcomer")
+    async def welcomer(self, interaction: discord.Interaction):
+        db.ensure_guild_exists(interaction.guild.id)
+        view = WelcomerPanel(self.bot, interaction.guild, interaction.user)
+        await interaction.response.send_message(view=view, ephemeral=True)
+
+
+async def setup(bot):
+    await bot.add_cog(WelcomerPanelCog(bot))
